062c77a6cf7bfc9a51b0a5699cd092ea95a7c444
SothPichPanha <sothpichpanhaedu@example.com>
2026-05-08 08:46:51 +0700

    done

--- src/graphql/resolvers/category.resolver.ts
+++ src/graphql/resolvers/category.resolver.ts
@@ -1,41 +1,41 @@
-// import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
-// import { CategoriesService } from '../../category/category.service';
-// import { Category } from 'src/category/entities/category.entity';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { CategoriesService } from '../../category/category.service';
+import { Category } from 'src/category/entities/category.entity';
 
-// @Resolver('Category')
-// export class CategoryResolver {
-//   constructor(private readonly categoriesService: CategoriesService) {}
+@Resolver('Category')
+export class CategoryResolver {
+  constructor(private readonly categoriesService: CategoriesService) {}
 
-//   @Query(() => [Category])  
-//   async getCategories() {
-//     return await this.categoriesService.findAll();
-//   }
+  @Query(() => [Category])  
+  async getCategories(id?: number) {
+    return await this.categoriesService.findAll();
+  }
 
-//   @Mutation('createCategory')
-//   async createCategory(
-//     @Args('name') name: string,
-//     @Args('productCount') productCount?: number,
-//     @Args('color') color?: string,
-//     @Args('image') image?: string,
-//   ) {
-//     try {
-//       const result = await this.categoriesService.create({
-//         name,
-//         productCount: productCount ?? 0,
-//         color: color ?? '',
-//         image: image ?? '',
-//       });
+  @Mutation('createCategory')
+  async createCategory(
+    @Args('name') name: string,
+    @Args('productCount') productCount?: number,
+    @Args('color') color?: string,
+    @Args('image') image?: string,
+  ) {
+    try {
+      const result = await this.categoriesService.create({
+        name,
+        productCount: productCount ?? 0,
+        color: color ?? '',
+        image: image ?? '',
+      });
       
-//       console.log('Created category result:', result);
+      console.log('Created category result:', result);
       
-//       if (!result) {
-//         throw new Error('Service returned null');
-//       }
+      if (!result) {
+        throw new Error('Service returned null');
+      }
       
-//       return result;
-//     } catch (error) {
-//       console.error('Error in createCategory:', error);
-//       throw error;
-//     }
-//   }
-// }
+      return result;
+    } catch (error) {
+      console.error('Error in createCategory:', error);
+      throw error;
+    }
+  }
+}
